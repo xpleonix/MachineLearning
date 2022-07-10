026ee5a51803effd216882b9553b95979e36ecf7
xpleonix <96414736+xpleonix@users.noreply.github.com>
2022-07-10 22:40:08 +0300

    Add training and prediction

--- SimpleLearning.py
+++ SimpleLearning.py
@@ -20,3 +20,16 @@ for i in range(train_set_count):
     
 for i in range(20):
     print(train_input[1], train_output[i])
+
+#training
+predictor = LinearRegression()
+predictor.fit(X=train_input, y=train_output)
+
+#prediction
+x_test = [[10, 20, 30]]
+outcome = predictor.predict(X=x_test)
+coefficients = predictor.coef_
+
+print('Outcome: ', outcome)
+print('Coefficients: ', coefficients)
+
