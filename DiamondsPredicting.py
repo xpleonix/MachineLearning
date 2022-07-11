baed27293a6bd8a96c4541c86c9184723164f8fa
xpleonix <96414736+xpleonix@users.noreply.github.com>
2022-07-11 10:28:46 +0300

    Add DiamondsPredicting.py

---
+++ DiamondsPredicting.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as snb
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('P1_diamonds.csv')
+
+print(df.head(10).to_string())
